f99dff380fda44ad9d1cb1a91abf3b611b29c28e
Maks Verver <maks@verver.ch>
2021-05-09 16:56:50 +0200

    Replace drag-and-drop with two-click movement.
    Drag-and-drop felt awkward and didn't work properly on mobile.

--- index.js
+++ index.js
@@ -22,18 +22,22 @@ function initialize() {
   const cells = [];   // cells[i] == div element of field `i`
   const adj = [];     // adj[i][d] == index of field adjacent to `i` in direction `d`, or -1 if none
   const middle = [];  // middle[i][j] == k, if k lies directly between `i` and `j`, or undefined if there is no such `k`
-  let goalIndex = -1; // index of goal field
 
-  let initialPegs;        // initial state: bitmask of pegs
-  let pegs;               // current state: bitmask of pegs
-  let draggedIndex = -1;  // index of peg currently being dragged
+  let goalIndex = -1; // index of goal field
+  let initialPegs;    // initial state: bitmask of pegs
+
+  // Variable state.
+  let pegs;             // current state: bitmask of pegs
+  let selectedPeg = -1; // index of peg selected to be moved
 
   const undoStack = [];
   const redoStack = [];
 
-  const memo = {};
+  const solvable = {};  // memoizes results of solve()
 
-  function Next(mask, i, dir) {
+  // Returns the next state after moving peg `i` in direction `dir`, or 0 if
+  // the move is not valid.
+  function getNext(mask, i, dir) {
     if (i >= 0 && (mask & (1 << i)) !== 0) {
       let k = adj[i][dir];
       if (k >= 0 && (mask & (1 << k)) !== 0) {
@@ -46,23 +50,23 @@ function initialize() {
     return 0;
   }
 
-  function Solve(pegs) {
-    let result = memo[pegs];
+  function solve(pegs) {
+    let result = solvable[pegs];
     if (result != undefined) return result;
     result = false;
   loop:
     for (let i = 0; (1 << i) <= pegs; ++i) {
       if ((pegs & (1 << i)) !== 0) {
         for (let d = 0; d < 8; ++d) {
-          let nextPegs = Next(pegs, i, d);
-          if (nextPegs !== 0 && Solve(nextPegs)) {
+          let nextPegs = getNext(pegs, i, d);
+          if (nextPegs !== 0 && solve(nextPegs)) {
             result = true;
             break loop;
           }
         }
       }
     }
-    memo[pegs] = result;
+    solvable[pegs] = result;
     return result;
   }
 
@@ -70,6 +74,7 @@ function initialize() {
     if (undoStack.length > 0) {
       redoStack.push(pegs);
       pegs = undoStack.pop();
+      selectedPeg = -1;
       updateState();
     }
   }
@@ -78,12 +83,14 @@ function initialize() {
     if (redoStack.length > 0) {
       undoStack.push(pegs);
       pegs = redoStack.pop();
+      selectedPeg = -1;
       updateState();
     }
   }
 
   function resetPegs() {
     pegs = initialPegs;
+    selectedPeg = -1;
     undoStack.length = 0;
     redoStack.length = 0;
     updateState();
@@ -93,81 +100,35 @@ function initialize() {
     undoStack.push(pegs);
     redoStack.length = 0;
     pegs = newPegs;
+    selectedPeg = -1;
     updateState();
   }
 
-  function removePeg(i) {
-    if (pegs !== initialPegs || (pegs & (1 << i)) === 0) return;
-    updatePegs(pegs - (1 << i));
-  }
-
-  function movePeg(i, j) {
-    if ((pegs & (1 << i)) === 0) return;
-    if ((pegs & (1 << j)) !== 0) return;
-    let k = middle[i][j];
-    if (k == null) return;
-    if ((pegs & (1 << k)) === 0) return;
-    updatePegs(pegs - (1 << i) + (1 << j) - (1 << k));
-  }
-
   function updateState() {
-    const pegMimeType = 'application/x-peg';
-    const firstMove = pegs === initialPegs;
     for (let i = 0; i < cells.length; ++i) {
       let cell = cells[i];
-      let peg = (pegs & (1 << i)) !== 0;
-      if (peg) {
+      if ((pegs & (1 << i)) !== 0) {
         cell.classList.remove('hole');
         cell.classList.add('peg');
+        if (selectedPeg === i) {
+          cell.classList.add('selected');
+        } else {
+          cell.classList.remove('selected');
+        }
       } else {
         cell.classList.remove('peg');
         cell.classList.add('hole');
-      }
-      if (peg && firstMove) {
-        cell.onclick = () => removePeg(i);
-      } else {
-        cell.onclick = undefined;
-      }
-      if (peg && !firstMove) {
-        cell.draggable = true;
-        cell.ondragstart = (ev) => {
-          ev.dataTransfer.setData(pegMimeType, String(i));
-          cell.classList.add('dragged');
-          draggedIndex = i;
-          updateHints();
-        };
-        cell.ondragend = (ev) => {
-          cell.classList.remove('dragged');
-          if (draggedIndex === i) draggedIndex = -1;
-          updateHints();
-        }
-        cell.ondragover = undefined;
-        cell.ondragleave = undefined;
-        cell.ondrop = undefined;
-      } else {
-        cell.draggable = false;
-        cell.ondragstart = undefined;
-        cell.ondragover = (ev) => {
-          // Note: can't check if this is *valid* drop target because we don't
-          // have access to event data until item is dropped.
-          cell.classList.add('droppable');
-          ev.preventDefault();  // necessary to accept drop
-        };
-        cell.ondragleave = (ev) => {
-          cell.classList.remove('droppable');
-        };
-        cell.ondrop = (ev) => {
-          cell.classList.remove('droppable');
-          let j = Number(ev.dataTransfer.getData(pegMimeType));
-          if (middle[i][j] != null) {
-            ev.preventDefault();
-            movePeg(j, i);
-          }
+        let k;
+        if (selectedPeg >= 0 && (k = middle[selectedPeg][i]) != null &&
+            (pegs & (1 << k)) !== 0) {
+          cell.classList.add('target');
+        } else {
+          cell.classList.remove('target');
         }
       }
     }
     updateHints();
-    resetButton.disabled = firstMove;
+    resetButton.disabled = pegs === initialPegs;
     undoButton.disabled = undoStack.length == 0;
     redoButton.disabled = redoStack.length == 0;
   }
@@ -175,14 +136,14 @@ function initialize() {
   function updateHints() {
     const hints = {};
     if (showSolutionCheckbox.checked) {
-      if (draggedIndex >= 0) {
-        let i = draggedIndex;
+      if (selectedPeg >= 0) {
+        let i = selectedPeg;
         for (let d = 0; d < 8; ++d) {
           let k = adj[i][d];
           if (k >= 0 && (pegs & (1 << k)) !== 0) {
             let j = adj[k][d];
             if (j >= 0 && (pegs & (1 << j)) == 0) {
-              if (Solve(pegs - (1 << i) + (1 << j) - (1 << k))) {
+              if (solve(pegs - (1 << i) + (1 << j) - (1 << k))) {
                 hints[j] = true;
               }
             }
@@ -193,8 +154,8 @@ function initialize() {
         for (let i = 0; i < cells.length; ++i) {
           if ((pegs & (1 << i)) !== 0) {
             for (let d = 0; d < 8; ++d) {
-              let next = Next(pegs, i, d);
-              if (next != null && Solve(next)) {
+              let next = getNext(pegs, i, d);
+              if (next != null && solve(next)) {
                 hints[i] = true;
                 break;
               }
@@ -213,18 +174,37 @@ function initialize() {
     }
   }
 
+  function onCellClicked(i) {
+    const peg = (pegs & (1 << i)) !== 0;
+    if (pegs === initialPegs) {
+      // Remove initial peg.
+      if (peg) updatePegs(pegs - (1 << i));
+    } else if (peg) {
+      // Select a starting peg (or unselect current one).
+      selectedPeg = selectedPeg !== i ? i : -1;
+      updateState();
+    } else if (selectedPeg >= 0) {
+      // Jump selected peg to the destination (if possible).
+      let k = middle[selectedPeg][i];
+      if (k != null && (pegs & (1 << k)) !== 0) {
+        updatePegs(pegs - (1 << selectedPeg) + (1 << i) - (1 << k));
+      }
+    }
+  }
+
   // Create cell elements. (Each cell is a hole or a peg.)
   for (let row = 0; row < H; ++row) {
     idx[row] = [];
     for (let col = 0; col < W; ++col) {
-      let ch = template[row][col];
+      const ch = template[row][col];
+      const i = cells.length;
       if (ch == ',') {
         const goalElem = document.createElement('div');
         goalElem.classList.add('goal');
         goalElem.style.gridRow = row + 1;
         goalElem.style.gridColumn = col + 1;
         gridElem.append(goalElem);
-        memo[1 << cells.length] = true;
+        solvable[1 << i] = true;
       }
       if (ch == '.' || ch == ',') {
         const cellElem = document.createElement('div');
@@ -232,8 +212,9 @@ function initialize() {
         cellElem.style.gridRow = row + 1;
         cellElem.style.gridColumn = col + 1;
         //cellElem.textContent = row + ',' + col;
+        cellElem.onclick = () => onCellClicked(i);
         gridElem.append(cellElem);
-        idx[row][col] = cells.length;
+        idx[row][col] = i;
         cells.push(cellElem);
       } else {
         idx[row][col] = -1;
@@ -277,7 +258,7 @@ function initialize() {
   let startTime = new Date().getTime();
   let zeros = 0, ones = 0;
   for (let i = 1, n = (1 << cells.length); i < n; ++i) {
-    if (Solve(i)) ++ones; else ++zeros;
+    if (solve(i)) ++ones; else ++zeros;
   }
   console.log(zeros, ones, (new Date().getTime() - startTime)/1000);
   // prints: 14050415 19504016 (~30 seconds)
